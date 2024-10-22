9b9c52762afaff008aa9c2c0c76039736288c96d
tranguyenn <nbtran.work@gmail.com>
2024-10-22 10:12:03 +0700

    fix update id

--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 // Validation for routes that require _id
 const validateId = [
-  param('_id')
-    .exists().withMessage('_id is required')
-    .custom(value => mongoose.Types.ObjectId.isValid(value)).withMessage('_id must be a valid MongoDB ObjectId')
+  param('id')
+    .exists().withMessage('id is required')
+    .custom(value => mongoose.Types.ObjectId.isValid(value)).withMessage('id must be a valid MongoDB ObjectId')
 ];
 
 module.exports = validateId;
